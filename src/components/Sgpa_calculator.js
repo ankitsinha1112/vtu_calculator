54b5828a410868541b80a93c7815cc9856de9191
Ankit Kumar Sinha <90057409+ankitsinha1112@users.noreply.github.com>
2021-10-26 14:55:28 +0530

    Add files via upload

---
+++ src/components/Sgpa_calculator.js
@@ -0,0 +1,92 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import './Sgpa_calculator.css'
+
+
+export const Sgpa_calculator = (props) => {
+    return (
+        <div>
+            <div className="head">
+                <h1>{props.Title}</h1>
+            </div>
+            <div className="subhead">
+                <h3>Subjects</h3>
+            </div>
+            <div className="marks">
+                <div className="sub">
+                    <div className="subname">
+                        <h5>{props.Sub1} -</h5>
+                    </div>
+                    <div className="form">
+                        <input type="number" placeholder = {props.Sub1} />
+                    </div>
+
+                </div>
+                <div className="sub">
+                    <div className="subname">
+                        <h5>{props.Sub2} -</h5>
+                    </div>
+                    <div className="form">
+                        <input type="number" />
+                    </div>
+
+                </div>
+                <div className="sub">
+                    <div className="subname">
+                        <h5>{props.Sub3} -</h5>
+                    </div>
+                    <div className="form">
+                        <input type="number" />
+                    </div>
+
+                </div>
+                <div className="sub">
+                    <div className="subname">
+                        <h5>{props.Sub4} -</h5>
+                    </div>
+                    <div className="form">
+                        <input type="number" />
+                    </div>
+
+                </div>
+                <div className="sub">
+                    <div className="subname">
+                        <h5>{props.Sub5} -</h5>
+                    </div>
+                    <div className="form">
+                        <input type="number" />
+                    </div>
+
+                </div>
+                <div className="sub">
+                    <div className="subname">
+                        <h5>{props.Sub6} -</h5>
+                    </div>
+                    <div className="form">
+                        <input type="number" />
+                    </div>
+
+                </div>
+                <div className="sub">
+                    <div className="subname">
+                        <h5>{props.Sub7} -</h5>
+                    </div>
+                    <div className="form">
+                        <input type="number" />
+                    </div>
+
+                </div>
+                <div className="sub">
+                    <div className="subname">
+                        <h5>{props.Sub8} -</h5>
+                    </div>
+                    <div className="form">
+                        <input type="number" />
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
