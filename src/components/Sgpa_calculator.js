86c3028be18ef6b2b9887b6c8acce0b0bc87fe72
dhomya vyas <dhomyavyas@gmail.com>
2022-01-07 20:25:09 +0530

    commit

--- src/components/Sgpa_calculator.js
+++ src/components/Sgpa_calculator.js
@@ -105,15 +105,15 @@ handleChange9=(event)=>{
 
 exe=(event)=>{
   event.preventDefault();
-  (this.state.m1==='100')? this.setState({x1:10}):this.setState({x1:Number(Math.floor(this.state.m1/10))+1});
-  (this.state.m2==='100')? this.setState({x2:10}):this.setState({x2:Number(Math.floor(this.state.m2/10))+1});
-  (this.state.m3==='100')? this.setState({x3:10}):this.setState({x3:Number(Math.floor(this.state.m3/10))+1});
-  (this.state.m4==='100')? this.setState({x4:10}):this.setState({x4:Number(Math.floor(this.state.m4/10))+1});
-  (this.state.m5==='100')? this.setState({x5:10}):this.setState({x5:Number(Math.floor(this.state.m5/10))+1});
-  (this.state.m6==='100')? this.setState({x6:10}):this.setState({x6:Number(Math.floor(this.state.m6/10))+1});
-  (this.state.m7==='100')? this.setState({x7:10}):this.setState({x7:Number(Math.floor(this.state.m7/10))+1});
-  (this.state.m8==='100')? this.setState({x8:10}):this.setState({x8:Number(Math.floor(this.state.m8/10))+1});
-  (this.state.m9==='100')? this.setState({x9:10}):this.setState({x9:Number(Math.floor(this.state.m9/10))+1});
+  (this.state.m1==='')?(this.setState({x1:0})):((this.state.m1==='100')? this.setState({x1:10}):this.setState({x1:Number(Math.floor(this.state.m1/10))+1}))
+  (this.state.m2==='')?(this.setState({x2:0})):((this.state.m2==='100')? this.setState({x2:10}):this.setState({x2:Number(Math.floor(this.state.m2/10))+1}))
+  (this.state.m3==='')?(this.setState({x3:0})):((this.state.m3==='100')? this.setState({x3:10}):this.setState({x3:Number(Math.floor(this.state.m3/10))+1}))
+  (this.state.m4==='')?(this.setState({x4:0})):((this.state.m4==='100')? this.setState({x4:10}):this.setState({x4:Number(Math.floor(this.state.m4/10))+1}))
+  (this.state.m5==='')?(this.setState({x5:0})):((this.state.m5==='100')? this.setState({x5:10}):this.setState({x5:Number(Math.floor(this.state.m5/10))+1}))
+  (this.state.m6==='')?(this.setState({x6:0})):((this.state.m6==='100')? this.setState({x6:10}):this.setState({x6:Number(Math.floor(this.state.m6/10))+1}))
+  (this.state.m7==='')?(this.setState({x7:0})):((this.state.m7==='100')? this.setState({x7:10}):this.setState({x7:Number(Math.floor(this.state.m7/10))+1}))
+  (this.state.m8==='')?(this.setState({x8:0})):((this.state.m8==='100')? this.setState({x8:10}):this.setState({x8:Number(Math.floor(this.state.m8/10))+1}))
+  (this.state.m9==='')?(this.setState({x9:0})):((this.state.m9==='100')? this.setState({x9:10}):this.setState({x9:Number(Math.floor(this.state.m9/10))+1}))
   this.setState({
         t2:(Number(this.state.c1)+Number(this.state.c2)+Number(this.state.c3)+Number(this.state.c4)+Number(this.state.c5)+Number(this.state.c6)+Number(this.state.c7)+Number(this.state.c8)+Number(this.state.c9))
   });
@@ -1100,25 +1100,14 @@ exe=(event)=>{
             </div>)
             :console.log('hello')
             }
-            {/* <h1>{this.state.t2}</h1> */}
-            {/* <h1>{this.state.x1*Number(this.state.c1)}</h1>
-            <h1>{this.state.x2*Number(this.state.c2)}</h1>
-            <h1>{this.state.x3*Number(this.state.c3)}</h1>
-            <h1>{this.state.x4*Number(this.state.c4)}</h1>
-            <h1>{this.state.x5*Number(this.state.c5)}</h1>
-            <h1>{this.state.x6*Number(this.state.c6)}</h1>
-            <h1>{this.state.x7*Number(this.state.c7)}</h1>
-            <h1>{this.state.x8*Number(this.state.c8)}</h1>
-    <h1>{this.state.t2}</h1> */}
             <div className="btn">
                                  <button  onClick={this.exe}>Submit</button>
             </div>
                                 {
                                 this.state.print?
-                                (<h1>{Number(Number((this.state.x1)*Number(this.state.c1)+(this.state.x2)*Number(this.state.c2)+(this.state.x3)*Number(this.state.c3)+(this.state.x4)*Number(this.state.c4)+(this.state.x5)*Number(this.state.c5)+(this.state.x6)*Number(this.state.c6)+(this.state.x7)*Number(this.state.c7)+(this.state.x8)*Number(this.state.c8)+(this.state.x9)*Number(this.state.c9))/this.state.t2).toFixed(2)}</h1>)
+                                (<h1 className='result'>Your SGPA : {Number(Number((this.state.x1)*Number(this.state.c1)+(this.state.x2)*Number(this.state.c2)+(this.state.x3)*Number(this.state.c3)+(this.state.x4)*Number(this.state.c4)+(this.state.x5)*Number(this.state.c5)+(this.state.x6)*Number(this.state.c6)+(this.state.x7)*Number(this.state.c7)+(this.state.x8)*Number(this.state.c8)+(this.state.x9)*Number(this.state.c9))/this.state.t2).toFixed(2)}</h1>)
                                 :(null)
-                                } 
-        
+                                }
           </div>
         </form>
       </div>
