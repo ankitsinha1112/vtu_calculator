f6e31895976f1210fe4fc58cf0131174055e572e
Ankit Kumar Sinha <ankitsinha111200@gmail.com>
2022-04-10 14:29:56 +0530

    commit

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./About.css";
-import dhomya from "../Images/dhomya.jpeg";
 import ankit from "../Images/ankit.png";
 
 export const About = () => {
@@ -15,18 +14,6 @@ export const About = () => {
         </p>
       </div>
       <div id="employee">
-        <div id="dhomya">
-          <img src={dhomya} alt="dhomya" />
-          <p>
-            A growing full stack web developer, who specializes in building web
-            applications for the best digital experiences. I am keenly focused
-            on building impactful products which can create value in people's
-            lives. Skilled in HTML, CSS, JavaScript , Express , Postman,
-            ReactJS, MongoDB, MySQL and node.js. I'm looking forward to
-            opportunities that will help me grow so that I keep contributing my
-            part in this beautiful field of technology.
-          </p>
-        </div>
         <div id="ankit">
           <img src={ankit} alt="ankit" />
           <p>
